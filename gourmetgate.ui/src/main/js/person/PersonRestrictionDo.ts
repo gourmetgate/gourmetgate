2e79b1604bfc0454cd8450d2993d79b3cfd6b916
Niklas Sidler <niklas.sidler@gmail.com>
2026-02-05 18:20:43 +0100

    19 Remove unused demo app files

--- gourmetgate.ui/src/main/js/person/PersonRestrictionDo.ts
+++
@@ -1,7 +0,0 @@
-import {BaseDoEntity, typeName} from '@eclipse-scout/core';
-
-@typeName('gourmetgate.PersonRestriction')
-export class PersonRestrictionDo extends BaseDoEntity {
-  firstName: string;
-  lastName: string;
-}
--- gourmetgate.ui/src/main/js/rest/AbstractItemResponse.ts
+++
@@ -1,5 +0,0 @@
-import {BaseDoEntity} from '@eclipse-scout/core';
-
-export abstract class AbstractItemResponse<TItem extends BaseDoEntity> extends BaseDoEntity {
-  items: TItem[];
-}
